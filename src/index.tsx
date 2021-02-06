7a165f69141ee4dff1ad23cdd9e56eb1800138da
KBL <kibinlee19k@gmail.com>
2021-02-07 01:17:36 +0900

    Initial Commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { ApolloProvider } from "@apollo/client";
+
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "./styles/styles.css";
+import { client } from "./apollo";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
