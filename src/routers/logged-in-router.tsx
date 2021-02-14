601942b2def1adc8647263d44017018dd38e3dff
KBL <kibinlee19k@gmail.com>
2021-02-14 17:10:13 +0900

    Header part Two

--- src/routers/logged-in-router.tsx
+++ src/routers/logged-in-router.tsx
@@ -7,8 +7,8 @@ import {
   Switch,
 } from "react-router-dom";
 import { Header } from "../components/header";
+import { useMe } from "../hooks/useMe";
 import { Restaurants } from "../pages/client/restaurants";
-import { meQuery } from "../__generated__/meQuery";
 
 const ClientRoutes = [
   <Route path="/" exact>
@@ -16,19 +16,9 @@ const ClientRoutes = [
   </Route>,
 ];
 
-const ME_QUERY = gql`
-  query meQuery {
-    me {
-      id
-      email
-      role
-      verified
-    }
-  }
-`;
-
 export const LoggedInRouter = () => {
-  const { data, loading, error } = useQuery<meQuery>(ME_QUERY);
+  const { data, loading, error } = useMe();
+
   if (!data || loading || error) {
     return (
       <div className="h-screen flex justify-center items-center">
@@ -41,7 +31,7 @@ export const LoggedInRouter = () => {
       <Header />
       <Switch>
         {data.me.role === "Client" && ClientRoutes}
-        <Redirect from="/potato" to="/" />
+        <Redirect to="/" />
       </Switch>
     </Router>
   );
