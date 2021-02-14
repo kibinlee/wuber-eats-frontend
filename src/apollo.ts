d9a79c0acd3d5ee27e185839a16edc3bffaf57f5
KBL <kibinlee19k@gmail.com>
2021-02-14 15:33:19 +0900

    Using the Token

--- src/apollo.ts
+++ src/apollo.ts
@@ -1,4 +1,10 @@
-import { ApolloClient, InMemoryCache, makeVar } from "@apollo/client";
+import {
+  ApolloClient,
+  InMemoryCache,
+  makeVar,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 import { LOCALSTORAGE_TOKEN } from "./constants";
 
 const token = localStorage.getItem(LOCALSTORAGE_TOKEN);
@@ -8,8 +14,21 @@ export const authTokenVar = makeVar(token);
 console.log("default value of isLoggedInVar is:", isLoggedInVar());
 console.log("default value of authTokenVar is:", authTokenVar());
 
-export const client = new ApolloClient({
+const httpLink = createHttpLink({
   uri: "http://localhost:4000/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      "x-jwt": authTokenVar() || "",
+    },
+  };
+});
+
+export const client = new ApolloClient({
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache({
     typePolicies: {
       Query: {
