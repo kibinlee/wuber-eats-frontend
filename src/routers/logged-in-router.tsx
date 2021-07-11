ec742dad1e88c4c25ef8c6fc3822962b4376cdcf
KBL <kibinlee19k@gmail.com>
2021-07-11 19:44:44 +0900

    Restaurant part One

--- src/routers/logged-in-router.tsx
+++ src/routers/logged-in-router.tsx
@@ -9,6 +9,7 @@ import { Restaurants } from "../pages/client/restaurants";
 import { Search } from "../pages/client/search";
 import { ConfirmEmail } from "../pages/user/confirm-email";
 import { EditProfile } from "../pages/user/edit-profile";
+import { Restaurant } from "../pages/client/restaurant";
 
 const ClientRoutes = [
   <Route key={1} path="/" exact>
@@ -25,6 +26,9 @@ const ClientRoutes = [
   </Route>,
   <Route key={5} path="/category/:slug">
     <Category />
+  </Route>,
+  <Route key={6} path="/restaurants/:id">
+    <Restaurant />
   </Route>,
 ];
 
